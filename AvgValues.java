91a351413aae9278dfd0a37ff46c3fb663c40c96
Vignesh-vicky37 <3vicky7@gmail.com>
2024-08-03 19:57:19 +0530

    Add files via upload

---
+++ AvgValues.java
@@ -0,0 +1,11 @@
+package org.array;
+
+public class AvgValues {
+public static void main(String[] args) {
+	
+	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	int avg;
+	avg=(a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9])/a.length;
+	System.out.println("Avg : "+ avg);
+}
+}
